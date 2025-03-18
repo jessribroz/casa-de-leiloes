a48ad9e4a7eab8a642a5a81aeb527e565d7a9366
jessribroz <thejeeh.ribeiro@gmail.com>
2025-03-18 08:28:51 -0300

    Adicionada classe Main para cadastroVIEW deve ser definida como a tela principal

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jessica
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Mostra a tela de cadastro
+        new cadastroVIEW().setVisible(true);  // 
+    }
+}
