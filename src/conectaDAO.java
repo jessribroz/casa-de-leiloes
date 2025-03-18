dee3bd1fae4a036c4959851e2adda47bd76ee851
jessribroz <thejeeh.ribeiro@gmail.com>
2025-03-18 10:30:33 -0300

    Alteração na classe conectaDAO

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,20 +1,8 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
     // Método para conectar ao banco de dados
@@ -23,9 +11,9 @@ public class conectaDAO {
         
         try {
             // URL do banco de dados, com usuário e senha para conexão
-            String url = "jdbc:mysql://localhost/uc11"; // Alterar conforme o nome do seu banco de dados
+            String url = "jdbc:mysql://localhost/uc11"; 
             String usuario = "root";  // Usuário do banco
-            String senha = "";        // Senha do banco (deixe vazio se não houver senha)
+            String senha = "Hpnxjr23!";        // Senha do banco
             
             // Estabelecendo a conexão
             conn = DriverManager.getConnection(url, usuario, senha);
